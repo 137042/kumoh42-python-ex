b11a74c1632cbe87e3e359270752ad6003fe2923
ChoiHyemin <mare137042@gmail.com>
2022-02-21 21:51:12 +0900

    Update main.py

--- hangman/main.py
+++ hangman/main.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
     try:
         game.getUserName()
     except ValueError:
+        # Hangman에서 입력받은 이름이 숫자라면 잘못된 입력으로 간주하기 위하여 float(name)이 실행됨
+        # int/float 아닐 경우 ValueError 발생함 -> 옳은 입력으로 간주하고 게임 실행되도록 try-except 설정
         game.execute()
     else:
        print("Invalid value")
